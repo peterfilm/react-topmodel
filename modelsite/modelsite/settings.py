1f611dd229310b8373ce8de3eff917fe9e5423d3
Peter Mikhaylutsa <petercash@mail.ru>
2024-06-03 01:19:56 +0300

    deloy to render.com

--- modelsite/modelsite/settings.py
+++ modelsite/modelsite/settings.py
@@ -164,6 +164,6 @@ if os.environ.get('RENDER'):
     DATABASES['default'] = dj_database_url.config(conn_max_age=600)
     # Безопасные параметры для продакшн
     DEBUG = False
-    ALLOWED_HOSTS = ['your-app-name.onrender.com']
+    ALLOWED_HOSTS = ['react-topmodel.onrender.com']
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
