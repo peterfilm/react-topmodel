0dfa8a292ed34f482a2366a7baefe2b475147487
Peter Mikhaylutsa <petercash@mail.ru>
2024-06-03 17:34:33 +0300

    media files from github

--- modelsite/modelsite/settings.py
+++ modelsite/modelsite/settings.py
@@ -144,7 +144,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_URL = '/media/'  # добавляет префикс к графическим файлам
+MEDIA_URL = 'https://raw.githubusercontent.com/peterfilm/react-topmodel/branch/media/'
+# MEDIA_URL = '/media/'  # добавляет префикс к графическим файлам
 
 
 STATIC_URL = '/static/'
@@ -181,4 +182,4 @@ if os.environ.get('RENDER'):
     STATIC_ROOT = BASE_DIR / 'staticfiles'
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     MEDIA_ROOT = BASE_DIR / 'media'
-    MEDIA_URL = '/media/' 
+    MEDIA_URL = 'https://raw.githubusercontent.com/peterfilm/react-topmodel/branch/media/' 
